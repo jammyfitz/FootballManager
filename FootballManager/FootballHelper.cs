0fb73d5dec1fc81a0889267057e7973ecbdfd4e9
jammyfitz:tyrian2 <jammyfitz@hotmail.com>
2014-02-18 12:24:36 +0000

    second commit through TFS

--- FootballManager/FootballHelper.cs
+++ FootballManager/FootballHelper.cs
@@ -185,7 +185,7 @@ namespace FootballManager
 
         public static string FormatStatsDataToText(DataView playerStats)
         {
-            string test = "a string";
+            string test = "a string change";
             string formattedData = "***MoleInTheBarn v1.0***\n";
 
             foreach (DataRowView playerStat in playerStats)
